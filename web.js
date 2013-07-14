a71c88e6a67559adcde1fb5f0312b628b49563f8
jgcoke@gmail.com <ubuntu@ip-172-31-47-158.us-west-2.compute.internal>
2013-07-14 12:57:52 +0000

    Fixed problem

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 //var outpout = fs.readFileSync('index.html');
 //buf.write(output);  
 //response.send(buf.toString());
-reponse.send('Hello World');
+response.send('Hello World');
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

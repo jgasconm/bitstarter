780e5918ff506c8be37d26ce49aa56089a0ce8ec
jgcoke@gmail.com <ubuntu@ip-172-31-47-158.us-west-2.compute.internal>
2013-07-14 13:07:45 +0000

    4

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var app = express.createServer(express.logger());
 var buf = new Buffer(256);
 
 app.get('/', function(request, response) {
-var output = fs.readFileSync('index.html');
-buf.write(output);  
+//var output = fs.readFileSync('index.html');
+buf.write(fs.readFileSync('index.html');  
 response.send(buf.toString());
-response.send('Hello World');
+//response.send('Hello World');
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

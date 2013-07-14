6fd65266d79b297f08803d9f6918e6b04029cb51
jgcoke@gmail.com <ubuntu@ip-172-31-47-158.us-west-2.compute.internal>
2013-07-14 12:54:51 +0000

    Modify web.js

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 var express = require('express');
-
+//var fs = require('fs');
 var app = express.createServer(express.logger());
+//var buf = new Buffer(256);
 
 app.get('/', function(request, response) {
-  response.send('Ojete calor!');
+//var outpout = fs.readFileSync('index.html');
+//buf.write(output);  
+//response.send(buf.toString());
+reponse.send('Hello World');
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
